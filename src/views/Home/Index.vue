14f970e33101c4732fc5caaa2f2ab70324424280
Thayana Mamoré <thayanacmamore@gmail.com>
2022-01-22 22:27:04 -0300

    :art: Criando tela inicial

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1 class="font-black text-brand-main">Aqui é a home</h1>
+    <custom-header />
+    <contact />
+    <div class="flex justify-center py-10 bg-brand-gray">
+        <p class="font-medium text-center text-gray-800">feedbacker © {{ new Date().getFullYear() }} </p>
+    </div>
 </template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (!token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {}
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
