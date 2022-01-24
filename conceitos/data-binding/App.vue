d889159d3dd32d22507042a7b7ae572295a1ae58
Thayana Mamoré <thayanacmamore@gmail.com>
2022-01-24 09:46:40 -0300

    :tada: Reforçando conceitos

---
+++ conceitos/data-binding/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello, {{name}}</h1>
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: 'Thayana'
+    })
+}
+</script>
+
+<style>
+
+</style>
---
+++ conceitos/lifecycle-hooks/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Lifecycle Hooks</h1>
+</template>
+
+<script>
+import { onMounted } from '@vue/runtime-core'
+export default {
+    setup() {
+        onMounted(() => {
+            console.log('Foi montado composition API');
+        })
+    }
+}
+
+/* export default {
+    mounted() {
+        console.log('Foi montado options API');
+    }
+} */
+</script>
+
+<style>
+
+</style>
