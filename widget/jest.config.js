2383c64e12e2e35c1d2e8d2bd3a42c9877530f7c
Thayana Mamoré <thayanacmamore@gmail.com>
2022-03-05 16:04:26 -0300

    🔧 Configurações para testes de unidades

--- widget/jest.config.js
+++ widget/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript',
+  testMatch: [
+    '**/*.spec.js'
+  ],
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
 }
